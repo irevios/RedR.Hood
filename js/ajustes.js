942067151b99b5efe2998989193f21d785edf5c9
irevios <irevios@gmail.com>
2020-02-26 01:59:51 +0100

    Audio fix

--- js/ajustes.js
+++ js/ajustes.js
@@ -1,26 +1,26 @@
 "use strict";
+    $("#volumen").slider({
+    value: 50,
+    orientation: "horizontal",
+    range: "min",
+    animate: true,
+    slide: function(event, ui) {
+        $(".valorVolumen").text(ui.value);
+        $("audio")[0].volume = ui.value / 100;
+        if (ui.value == 0) {
+            $("audio")[0].pause();
+            $("#playMusic i:not('.fa-music')").switchClass("fa-pause", "fa-play");
+        }
+        if (ui.value > 0) {
+            $("audio")[0].play();
+            $("#playMusic i:not('.fa-music')").switchClass("fa-play", "fa-pause");
+        }
+    }
+});
 $(".ajustes").click(function() {
     $("<i class='fa fa-close' aria-hidden='true'></i>").insertBefore(".ui-dialog-titlebar.ui-corner-all.ui-widget-header.ui-helper-clearfix.ui-draggable-handle>button");
     $(".ui-dialog-titlebar.ui-corner-all.ui-widget-header.ui-helper-clearfix.ui-draggable-handle>button").remove();
     $(".fa-close").click(function() { $("#dialog").dialog("close") });
-    $("#volumen").slider({
-        value: 50,
-        orientation: "horizontal",
-        range: "min",
-        animate: true,
-        slide: function(event, ui) {
-            $(".valorVolumen").text(ui.value);
-            $("audio").attr("volume", ui.value / 100);
-            if (ui.value == 0) {
-                $("audio")[0].pause();
-                $("#playMusic i:not('.fa-music')").switchClass("fa-pause", "fa-play");
-            }
-            if (ui.value > 0) {
-                $("audio")[0].play();
-                $("#playMusic i:not('.fa-music')").switchClass("fa-play", "fa-pause");
-            }
-        }
-    });
     $(".derecha span").text(teclasMovimiento.derecha.nom);
     $(".izquierda span").text(teclasMovimiento.izquierda.nom);
     $(".saltar span").text(teclasMovimiento.saltar.nom);
@@ -38,7 +38,6 @@ $("#playMusic").click(function() {
     if ($("#playMusic i:not('.fa-music')").hasClass("fa-pause")) {
         $("#playMusic i:not('.fa-music')").switchClass("fa-pause", "fa-play");
         $("audio")[0].pause();
-        $("audio").attr("volume", 50 / 100);
     } else {
         $("#playMusic i:not('.fa-music')").switchClass("fa-play", "fa-pause");
         $("audio")[0].play();
